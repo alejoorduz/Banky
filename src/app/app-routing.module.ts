e84ef12a4437fb0247c4f0bb162e04f2d6775190
Alejandro Orduz <alejorduz@gmail.com>
2021-09-08 19:13:42 -0500

    pages modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,8 @@ import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: 'ingresoadmin',
-    loadChildren: () => import('./ingresoadmin/ingresoadmin.module').then( m => m.IngresoadminPageModule)
+    loadChildren: () => import('./iniciosesion/ingresoadmin.module').then( m => m.IngresoadminPageModule)
   },
   {
     path: '',
@@ -18,20 +14,20 @@ const routes: Routes = [
   },
   {
     path: 'details',
-    loadChildren: () => import('./details/details.module').then( m => m.DetailsPageModule)
+    loadChildren: () => import('./recuperacion/details.module').then( m => m.DetailsPageModule)
   },
   {
     path: 'inicio',
-    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
+    loadChildren: () => import('./verificacion/inicio.module').then( m => m.InicioPageModule)
   },
    {
     path: 'admin',
-    loadChildren: () => import('./admin/admin.module').then( m => m.AdminPageModule),
+    loadChildren: () => import('./corriente/admin.module').then( m => m.AdminPageModule),
     canActivate:[AuthGuard],
   },
   {
     path: 'adicionar',
-    loadChildren: () => import('./adicionar/adicionar.module').then( m => m.AdicionarPageModule)
+    loadChildren: () => import('./generarQR/adicionar.module').then( m => m.AdicionarPageModule)
   },
    {
     path: 'register',
@@ -44,11 +40,11 @@ const routes: Routes = [
   },
   {
     path: 'demanda',
-    loadChildren: () => import('./demanda/demanda.module').then( m => m.DemandaPageModule)
+    loadChildren: () => import('./ahorros/demanda.module').then( m => m.DemandaPageModule)
   },
   {
     path: 'adicionardemanda',
-    loadChildren: () => import('./adicionardemanda/adicionardemanda.module').then( m => m.AdicionardemandaPageModule)
+    loadChildren: () => import('./escanearQR/adicionardemanda.module').then( m => m.AdicionardemandaPageModule)
   },
 ];
 
